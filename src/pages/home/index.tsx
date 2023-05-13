192a84c791c870e40aa73725bed06a28c9b6f3b9
DSDmark <sahup3296@gmail.com>
2023-05-14 00:53:37 +0530

    video added

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,32 @@
+import { styled } from '@mui/material/styles';
+import videoBg from "/videos/background.mp4"
+import { Box, Grid } from '@mui/material';
+
+const VideoBackground = styled('div')({
+  width: "100%",
+  marginBottom: -10,
+  overflow: "hidden",
+  position: "relative",
+  "& video": {
+    height: "auto",
+    width: '100%',
+  }
+});
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box>
+      <VideoBackground>
+        <video controls={false} autoPlay={true} loop={true}>
+          <source src={videoBg} type="video/mp4" />
+        </video>
+      </VideoBackground>
+      <Grid container >
+        <Grid item>
+          csd
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
 
